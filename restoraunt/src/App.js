c84e47ad1c59f45e9083d71b294ae2f29f175a20
Dimitri <dimak821@gmail.com>
2022-10-06 00:55:29 +0200

    start

---
+++ restoraunt/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Navbar from './components/navbar';
+
+
+function App() {
+  return (
+    <>
+    
+    <Navbar />
+
+    <div className='container'>
+      <h1>Welcome to Elite</h1>
+    </div>
+    
+    
+    </>
+    
+
+
+  );
+}
+
+export default App;
---
+++ restoraunt/src/components/navbar.js
@@ -0,0 +1,23 @@
+
+
+function Navbar(){
+    return(
+    <nav className="navbar  bg-dark">
+    <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">Elite</a>
+    </div>
+    </nav>
+    );
+}
+
+
+
+export default Navbar;
+
+
+
+
+
+
+
+
